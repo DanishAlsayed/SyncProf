eca4adc66f1fd1efd5015381569cf2c8229d916f
DanishAlsayed <danish.alsayed@gmail.com>
2017-01-16 01:45:40 +0800

    First push - skeleton of pass implemented

---
+++ Makefile
@@ -0,0 +1,5 @@
+LEVEL = ../../../../../..
+LIBRARYNAME = SyncProf
+LOADABLE_MODULE = 1
+include $(LEVEL)/Makefile.common
+
---
+++ Release+Asserts/SyncProf.d
@@ -0,0 +1,228 @@
+/home/danish/Desktop/Owl/llvm/llvm-3.4/lib/Transforms/SyncProf/testing/llvm-pass-skeleton-rtlib/SyncProf/Release+Asserts/SyncProf.o \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/lib/Transforms/SyncProf/testing/llvm-pass-skeleton-rtlib/SyncProf/Release+Asserts/SyncProf.d: \
+  SyncProf.cpp \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Pass.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Compiler.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Config/llvm-config.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/PassSupport.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/InitializePasses.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/PassRegistry.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/StringRef.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/type_traits.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/DataTypes.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/CBindingWrapping.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Casting.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm-c/Core.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Atomic.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Valgrind.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/PassAnalysisSupport.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/SmallVector.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/AlignOf.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/MathExtras.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/SwapByteOrder.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Function.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Argument.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/Twine.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/ErrorHandling.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/ilist_node.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Attributes.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/ArrayRef.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/None.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/FoldingSet.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/PointerLikeTypeTraits.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Value.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Use.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/PointerIntPair.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/BasicBlock.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/ilist.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Instruction.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/User.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/DebugLoc.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Instruction.def \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/SymbolTableListTraits.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/CallingConv.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/GlobalValue.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Constant.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/DerivedTypes.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Type.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/APFloat.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/APInt.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/raw_ostream.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/FileSystem.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/IntrusiveRefCntPtr.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/OwningPtr.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/SmallString.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/TimeValue.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/system_error.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/LegacyPassManager.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/InstrTypes.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/OperandTraits.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Transforms/IPO/PassManagerBuilder.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/IRBuilder.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/DataLayout.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/DenseMap.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/DenseMapInfo.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Instructions.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/LLVMContext.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Operator.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Constants.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/GetElementPtrTypeIterator.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/ConstantFolder.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/ValueHandle.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Transforms/Utils/BasicBlockUtils.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/CFG.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/GraphTraits.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Module.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/GlobalAlias.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/GlobalVariable.h \
+  /home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Metadata.h
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Pass.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Compiler.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Config/llvm-config.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/PassSupport.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/InitializePasses.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/PassRegistry.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/StringRef.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/type_traits.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/DataTypes.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/CBindingWrapping.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Casting.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm-c/Core.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Atomic.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/Valgrind.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/PassAnalysisSupport.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/SmallVector.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/AlignOf.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/MathExtras.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/SwapByteOrder.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Function.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Argument.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/Twine.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/ErrorHandling.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/ilist_node.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Attributes.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/ArrayRef.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/None.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/FoldingSet.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/PointerLikeTypeTraits.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Value.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Use.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/PointerIntPair.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/BasicBlock.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/ilist.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Instruction.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/User.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/DebugLoc.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Instruction.def:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/SymbolTableListTraits.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/CallingConv.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/GlobalValue.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Constant.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/DerivedTypes.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Type.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/APFloat.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/APInt.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/raw_ostream.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/FileSystem.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/IntrusiveRefCntPtr.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/OwningPtr.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/SmallString.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/TimeValue.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/system_error.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/LegacyPassManager.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/InstrTypes.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/OperandTraits.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Transforms/IPO/PassManagerBuilder.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/IRBuilder.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/DataLayout.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/DenseMap.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/DenseMapInfo.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Instructions.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/LLVMContext.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Operator.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Constants.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/GetElementPtrTypeIterator.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/ConstantFolder.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/ValueHandle.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Transforms/Utils/BasicBlockUtils.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/Support/CFG.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/ADT/GraphTraits.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Module.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/GlobalAlias.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/GlobalVariable.h:
+
+/home/danish/Desktop/Owl/llvm/llvm-3.4/include/llvm/IR/Metadata.h:
